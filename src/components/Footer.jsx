b74b3bc61b05aa86c600cd64404308b5854cca27
Keadon Mitchell <mitchell.keadon@outlook.com>
2023-11-25 21:24:37 -0600

    change footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import '../css/footer.scss';
 function Footer() {
   return (
     <footer>
-      <a id="copyright" href="https://github.com/keadonm" target="_blank">
+      <a
+        id="copyright"
+        href="https://github.com/KeadonM/13-odin-cv-application"
+        target="_blank">
         Copyright&nbsp;<span id="copyright-logo">©</span>&nbsp;
         <span>{new Date().getFullYear()}</span>&nbsp;KeadonM&nbsp;
         <i className="fab fa-github"></i>
